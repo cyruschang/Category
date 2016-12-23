49b688ed2e3926dd4bbed23d1da08b0d9ecb67bd
cyruschang <changluyang@changluyangdeMacBook-Pro.local>
2016-12-23 10:52:48 +0800

    UIView类别
    UIView类别

---
+++ UIView/UIView+Extension.h
@@ -0,0 +1,21 @@
+//
+//  UIView+Extension.h
+//  UIView 类别
+//
+//  Created by Chang on 2016/12/23.
+//  Copyright © 2016年 ChangLuyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (Extension)
+
+@property (nonatomic, assign) CGFloat x;
+@property (nonatomic, assign) CGFloat y;
+@property (nonatomic, assign) CGFloat centerX;
+@property (nonatomic, assign) CGFloat centerY;
+@property (nonatomic, assign) CGFloat width;
+@property (nonatomic, assign) CGFloat height;
+@property (nonatomic, assign) CGSize size;
+
+@end
